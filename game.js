8de77c595151901355fb068512e1d4a9ef7671ab
uwusenpaidaddy6741-spec <uwusenpaidaddy6741@gmail.com>
2026-09-10 21:10:07 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2521,6 +2521,7 @@ let raceElapsedTime = 0;
 // ============================================================
 
 let raceStarted = false;
+let trackSelected = false;
 
 let countdownTime = 3;
 
@@ -2586,6 +2587,10 @@ container.appendChild(
 // ------------------------------------------------------------
 
 function updateCountdown(deltaTime) {
+
+        if (!trackSelected) {
+        return;
+    }
 
     if (countdownFinished) {
         return;
