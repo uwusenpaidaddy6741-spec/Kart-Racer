7fba58f9ee13993569c43d07c37c115749272137
uwusenpaidaddy6741-spec <uwusenpaidaddy6741@gmail.com>
2026-09-12 14:43:52 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,9 @@
-
+export default {
+    async fetch(request, env) {
+        return new Response("Kart Racer API is online!", {
+            headers: {
+                "Content-Type": "text/plain"
+            }
+        });
+    }
+};
