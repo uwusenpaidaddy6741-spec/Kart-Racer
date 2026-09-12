6a9131aa730993f7eba5f3e29afe422122b264b4
uwusenpaidaddy6741-spec <uwusenpaidaddy6741@gmail.com>
2026-09-12 14:54:36 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,12 +2,23 @@ export default {
     async fetch(request, env) {
         const url = new URL(request.url);
 
-        if (url.pathname.startsWith("/api/")) {
-            return new Response("API is working!", {
-                headers: {
-                    "Content-Type": "text/plain"
-                }
-            });
+        if (url.pathname === "/api/leaderboard") {
+            const track = url.searchParams.get("track");
+
+            if (!track) {
+                return new Response("Missing track", {
+                    status: 400
+                });
+            }
+
+            const result = await env.MY_DB
+                .prepare(
+                    "SELECT name, time FROM leaderboard WHERE track = ? ORDER BY time ASC LIMIT 5"
+                )
+                .bind(track)
+                .all();
+
+            return Response.json(result.results);
         }
 
         return env.ASSETS.fetch(request);
