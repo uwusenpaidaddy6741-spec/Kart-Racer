2e1b262f167abbf7e4a6fa0890f7e300024e6207
uwusenpaidaddy6741-spec <uwusenpaidaddy6741@gmail.com>
2026-09-12 15:07:50 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,26 +2,6 @@ export default {
     async fetch(request, env) {
         const url = new URL(request.url);
 
-        // GET leaderboard
-        if (url.pathname === "/api/leaderboard") {
-            const track = url.searchParams.get("track");
-
-            if (!track) {
-                return new Response("Missing track", {
-                    status: 400
-                });
-            }
-
-            const result = await env.MY_DB
-                .prepare(
-                    "SELECT name, time FROM leaderboard WHERE track = ? ORDER BY time ASC LIMIT 5"
-                )
-                .bind(track)
-                .all();
-
-            return Response.json(result.results);
-        }
-
         // POST new leaderboard score
         if (
             url.pathname === "/api/leaderboard" &&
@@ -60,6 +40,29 @@ export default {
                     status: 400
                 });
             }
+        }
+
+        // GET leaderboard
+        if (
+            url.pathname === "/api/leaderboard" &&
+            request.method === "GET"
+        ) {
+            const track = url.searchParams.get("track");
+
+            if (!track) {
+                return new Response("Missing track", {
+                    status: 400
+                });
+            }
+
+            const result = await env.MY_DB
+                .prepare(
+                    "SELECT name, time FROM leaderboard WHERE track = ? ORDER BY time ASC LIMIT 5"
+                )
+                .bind(track)
+                .all();
+
+            return Response.json(result.results);
         }
 
         return env.ASSETS.fetch(request);
