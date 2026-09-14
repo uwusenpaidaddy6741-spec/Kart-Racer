052523d88fbe99624e3277fd261d698520095a2f
uwusenpaidaddy6741-spec <uwusenpaidaddy6741@gmail.com>
2026-09-14 18:42:45 -0400

    Update game.js

--- game.js
+++ game.js
@@ -698,9 +698,15 @@ const kartStats = {
         braking: 30,
         turnSpeed: 3.5,
         driftChargeRate: 1.5
-    }
-
-};
+    },
+        
+    blaze: {
+    maxSpeed: 46,
+    acceleration: 22,
+    braking: 20,
+    turnSpeed: 2.5,
+    driftChargeRate: 1.2
+},
 
 const kartButtons =
     document.querySelectorAll(
@@ -785,13 +791,6 @@ const wheelStats = {
         turnSpeed: 0.3,
         driftChargeRate: 1.5
     }
-
-    blaze: {
-    maxSpeed: 46,
-    acceleration: 22,
-    braking: 20,
-    turnSpeed: 2.5,
-    driftChargeRate: 1.2
 },
 
 const wheelButtons =
